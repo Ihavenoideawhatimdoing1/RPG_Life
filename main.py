26c9d277147ada5eb134f7683479cec086c7d9d5
Ihavenoideawhatimdoing1 <146613255+Ihavenoideawhatimdoing1@users.noreply.github.com>
2024-07-11 13:50:36 +0000

    Pending changes exported from your codespace

---
+++ main.py
@@ -0,0 +1,387 @@
+import pandas as pd #imports zoo animal to write code
+import time
+
+
+#checks the level_path file too see if you leveled up
+def check_new_title():
+    if number_of_dots == 20:
+        print(data.at[0, "Title"])
+        print(data.at[0, "Message"])
+    elif number_of_dots == 40:
+        print(data.at[1, "Title"])
+        print(data.at[1, "Message"])
+    elif number_of_dots == 60:
+        print(data.at[2, "Title"])
+        print(data.at[2, "Message"])
+    elif number_of_dots == 80:
+        print(data.at[3, "Title"])
+        print(data.at[3, "Message"])
+    elif number_of_dots == 100:
+        print(data.at[4, "Title"])
+        print(data.at[4, "Message"])
+    elif number_of_dots == 130:
+        print(data.at[5, "Title"])
+        print(data.at[5, "Message"])
+    elif number_of_dots == 160:
+        print(data.at[6, "Title"])
+        print(data.at[6, "Message"])
+    elif number_of_dots == 190:
+        print(data.at[7, "Title"])
+        print(data.at[7, "Message"])
+    elif number_of_dots == 220:
+        print(data.at[8, "Title"])
+        print(data.at[8, "Message"])
+    elif number_of_dots == 250:
+        print(data.at[9, "Title"])
+        print(data.at[9, "Message"])
+    elif number_of_dots == 290:
+        print(data.at[10, "Title"])
+        print(data.at[10, "Message"])
+    elif number_of_dots == 330:
+        print(data.at[11, "Title"])
+        print(data.at[11, "Message"])
+    elif number_of_dots == 370:
+        print(data.at[12, "Title"])
+        print(data.at[12, "Message"])
+    elif number_of_dots == 410:
+        print(data.at[13, "Title"])
+        print(data.at[13, "Message"])
+    elif number_of_dots == 450:
+        print(data.at[14, "Title"])
+        print(data.at[14, "Message"])
+    elif number_of_dots == 500:
+        print(data.at[15, "Title"])
+        print(data.at[15, "Message"])
+    elif number_of_dots == 550:
+        print(data.at[16, "Title"])
+        print(data.at[16, "Message"])
+    elif number_of_dots == 600:
+        print(data.at[17, "Title"])
+        print(data.at[17, "Message"])
+    elif number_of_dots == 650:
+        print(data.at[18, "Title"])
+        print(data.at[18, "Message"])
+    elif number_of_dots == 700:
+        print(data.at[19, "Title"])
+        print(data.at[19, "Message"])
+    elif number_of_dots == 760:
+        print(data.at[20, "Title"])
+        print(data.at[20, "Message"])
+    elif number_of_dots == 820:
+        print(data.at[21, "Title"])
+        print(data.at[21, "Message"])
+    elif number_of_dots == 880:
+        print(data.at[22, "Title"])
+        print(data.at[22, "Message"])
+    elif number_of_dots == 940:
+        print(data.at[23, "Title"])
+        print(data.at[23, "Message"])
+    elif number_of_dots == 1000:
+        print(data.at[24, "Title"])
+        print(data.at[24, "Message"])
+    elif number_of_dots == 1070:
+        print(data.at[25, "Title"])
+        print(data.at[25, "Message"])
+    elif number_of_dots == 1140:
+        print(data.at[26, "Title"])
+        print(data.at[26, "Message"])
+    elif number_of_dots == 1210:
+        print(data.at[27, "Title"])
+        print(data.at[27, "Message"])
+    elif number_of_dots == 1280:
+        print(data.at[28, "Title"])
+        print(data.at[28, "Message"])
+    elif number_of_dots == 1350:
+        print(data.at[29, "Title"])
+        print(data.at[29, "Message"])
+    elif number_of_dots == 1430:
+        print(data.at[30, "Title"])
+        print(data.at[30, "Message"])
+    elif number_of_dots == 1510:
+        print(data.at[31, "Title"])
+        print(data.at[31, "Message"])
+    elif number_of_dots == 1590:
+        print(data.at[32, "Title"])
+        print(data.at[32, "Message"])
+    elif number_of_dots == 1670:
+        print(data.at[33, "Title"])
+        print(data.at[33, "Message"])
+    elif number_of_dots == 1750:
+        print(data.at[34, "Title"])
+        print(data.at[34, "Message"])
+    elif number_of_dots == 1840:
+        print(data.at[35, "Title"])
+        print(data.at[35, "Message"])
+    elif number_of_dots == 1930:
+        print(data.at[36, "Title"])
+        print(data.at[36, "Message"])
+    elif number_of_dots == 2020:
+        print(data.at[37, "Title"])
+        print(data.at[37, "Message"])
+    elif number_of_dots == 2110:
+        print(data.at[38, "Title"])
+        print(data.at[38, "Message"])
+    elif number_of_dots == 2200:
+        print(data.at[39, "Title"])
+        print(data.at[39, "Message"])
+    elif number_of_dots == 2300:
+        print(data.at[40, "Title"])
+        print(data.at[40, "Message"])
+    elif number_of_dots == 2400:
+        print(data.at[41, "Title"])
+        print(data.at[41, "Message"])
+    elif number_of_dots == 2500:
+        print(data.at[42, "Title"])
+        print(data.at[42, "Message"])
+    elif number_of_dots == 2600:
+        print(data.at[43, "Title"])
+        print(data.at[43, "Message"])
+    elif number_of_dots == 2700:
+        print(data.at[44, "Title"])
+        print(data.at[44, "Message"])
+    elif number_of_dots == 2810:
+        print(data.at[45, "Title"])
+        print(data.at[45, "Message"])
+    elif number_of_dots == 2920:
+        print(data.at[46, "Title"])
+        print(data.at[46, "Message"])
+    elif number_of_dots == 3030:
+        print(data.at[47, "Title"])
+        print(data.at[47, "Message"])
+    elif number_of_dots == 3140:
+        print(data.at[48, "Title"])
+        print(data.at[48, "Message"])
+    elif number_of_dots == 3250:
+        print(data.at[49, "Title"])
+        print(data.at[49, "Message"])
+    elif number_of_dots == 3370:
+        print(data.at[50, "Title"])
+        print(data.at[50, "Message"])
+    elif number_of_dots == 3490:
+        print(data.at[51, "Title"])
+        print(data.at[51, "Message"])
+    elif number_of_dots == 3610:
+        print(data.at[52, "Title"])
+        print(data.at[52, "Message"])
+    elif number_of_dots == 3730:
+        print(data.at[53, "Title"])
+        print(data.at[53, "Message"])
+    elif number_of_dots == 3850:
+        print(data.at[54, "Title"])
+        print(data.at[54, "Message"])
+    elif number_of_dots == 3980:
+        print(data.at[55, "Title"])
+        print(data.at[55, "Message"])
+    elif number_of_dots == 4110:
+        print(data.at[56, "Title"])
+        print(data.at[56, "Message"])
+    elif number_of_dots == 4240:
+        print(data.at[57, "Title"])
+        print(data.at[57, "Message"])
+    elif number_of_dots == 4370:
+        print(data.at[58, "Title"])
+        print(data.at[58, "Message"])
+    elif number_of_dots == 4500:
+        print(data.at[59, "Title"])
+        print(data.at[59, "Message"])
+    elif number_of_dots == 4640:
+        print(data.at[60, "Title"])
+        print(data.at[60, "Message"])
+    elif number_of_dots == 4780:
+        print(data.at[61, "Title"])
+        print(data.at[61, "Message"])
+    elif number_of_dots == 4920:
+        print(data.at[62, "Title"])
+        print(data.at[62, "Message"])
+    elif number_of_dots == 5060:
+        print(data.at[63, "Title"])
+        print(data.at[63, "Message"])
+    elif number_of_dots == 5200:
+        print(data.at[64, "Title"])
+        print(data.at[64, "Message"])
+    elif number_of_dots == 5350:
+        print(data.at[65, "Title"])
+        print(data.at[65, "Message"])
+    elif number_of_dots == 5500:
+        print(data.at[66, "Title"])
+        print(data.at[66, "Message"])
+    elif number_of_dots == 5650:
+        print(data.at[67, "Title"])
+        print(data.at[67, "Message"])
+    elif number_of_dots == 5800:
+        print(data.at[68, "Title"])
+        print(data.at[68, "Message"])
+    elif number_of_dots == 5950:
+        print(data.at[69, "Title"])
+        print(data.at[69, "Message"])
+    elif number_of_dots == 6110:
+        print(data.at[70, "Title"])
+        print(data.at[70, "Message"])
+    elif number_of_dots == 6270:
+        print(data.at[71, "Title"])
+        print(data.at[71, "Message"])
+    elif number_of_dots == 6430:
+        print(data.at[72, "Title"])
+        print(data.at[72, "Message"])
+    elif number_of_dots == 6590:
+        print(data.at[73, "Title"])
+        print(data.at[73, "Message"])
+    elif number_of_dots == 6750:
+        print(data.at[74, "Title"])
+        print(data.at[74, "Message"])
+    elif number_of_dots == 6920:
+        print(data.at[75, "Title"])
+        print(data.at[75, "Message"])
+    elif number_of_dots == 7090:
+        print(data.at[76, "Title"])
+        print(data.at[76, "Message"])
+    elif number_of_dots == 7260:
+        print(data.at[77, "Title"])
+        print(data.at[77, "Message"])
+    elif number_of_dots == 7430:
+        print(data.at[78, "Title"])
+        print(data.at[78, "Message"])
+    elif number_of_dots == 7600:
+        print(data.at[79, "Title"])
+        print(data.at[79, "Message"])
+    elif number_of_dots == 7780:
+        print(data.at[80, "Title"])
+        print(data.at[80, "Message"])
+    elif number_of_dots == 7960:
+        print(data.at[81, "Title"])
+        print(data.at[81, "Message"])
+    elif number_of_dots == 8140:
+        print(data.at[82, "Title"])
+        print(data.at[82, "Message"])
+    elif number_of_dots == 8320:
+        print(data.at[83, "Title"])
+        print(data.at[83, "Message"])
+    elif number_of_dots == 8500:
+        print(data.at[84, "Title"])
+        print(data.at[84, "Message"])
+    elif number_of_dots == 8690:
+        print(data.at[85, "Title"])
+        print(data.at[85, "Message"])
+    elif number_of_dots == 8880:
+        print(data.at[86, "Title"])
+        print(data.at[86, "Message"])
+    elif number_of_dots == 9070:
+        print(data.at[87, "Title"])
+        print(data.at[87, "Message"])
+    elif number_of_dots == 9260:
+        print(data.at[88, "Title"])
+        print(data.at[88, "Message"])
+    elif number_of_dots == 9450:
+        print(data.at[89, "Title"])
+        print(data.at[89, "Message"])
+    elif number_of_dots == 9650:
+        print(data.at[90, "Title"])
+        print(data.at[90, "Message"])
+    elif number_of_dots == 9850:
+        print(data.at[91, "Title"])
+        print(data.at[91, "Message"])
+    elif number_of_dots == 10050:
+        print(data.at[92, "Title"])
+        print(data.at[92, "Message"])
+    elif number_of_dots == 10250:
+        print(data.at[93, "Title"])
+        print(data.at[93, "Message"])
+    elif number_of_dots == 10450:
+        print(data.at[94, "Title"])
+        print(data.at[94, "Message"])
+    elif number_of_dots == 10660:
+        print(data.at[95, "Title"])
+        print(data.at[95, "Message"])
+    elif number_of_dots == 10870:
+        print(data.at[96, "Title"])
+        print(data.at[96, "Message"])
+    elif number_of_dots == 11080:
+        print(data.at[97, "Title"])
+        print(data.at[97, "Message"])
+    elif number_of_dots == 11290:
+        print(data.at[98, "Title"])
+        print(data.at[98, "Message"])
+    elif number_of_dots == 11500:
+        print(data.at[99, "Title"])
+        print(data.at[99, "Message"])
+    elif number_of_dots > 11500:
+        print('You beat the game dude... Stop bugging me.')
+        menu()
+    else:
+        print(f'You have', number_of_dots, "points... come back when you've gained enough points to warrant my attention.")
+        menu()
+
+#counts the number of points you have
+def count_points(filename):
+  """
+  This function opens a text file and counts the number of dots (periods).
+
+  Args:
+      filename: The path to the text file.
+
+  Returns:
+      The number of dots found in the file.
+  """
+  point_count = 0
+  with open(filename, "r") as file:
+    for line in file:
+      point_count += line.count("x_")
+  return point_count
+
+#files you'll need
+level_path = "RPG_LIFE\\Leveling_Table\\Level_Table - Sheet1.csv" #Replace with your path
+filename = 'Points Counter\\Points_txt\\Writing_Points.txt'  # Replace with your to_do file
+titles = "RPG_LIFE\\Leveling_Table\\Titles.txt" #Replace with your path
+
+#reading the to do list in your txt file
+with open(titles, 'r') as file:
+  titles_content = file.read()
+#counting the number of points you have
+number_of_dots = count_points(filename)
+#reading the level progression csv
+data = pd.read_csv(level_path)
+#inital navigation menu
+
+def menu():
+    print("""
+        
+        Would you like to: 
+        (A): Check your points
+        (B): Check number of points needed to level up for each level
+        (C): Check to see if you leveled up
+        (D): Discover the meaning of life 
+        (E): Quit
+          
+        """)
+    nav_menu_input = input(': ')
+    if nav_menu_input.lower() == 'a':
+        time.sleep(.5)
+        print(f'You have', number_of_dots, 'Congrats I guess...')
+        time.sleep(1)
+        menu()
+    elif nav_menu_input.lower() == 'b':
+        time.sleep(.5)
+        print(f'You have', number_of_dots, "points.")
+        time.sleep(.5)
+        print(titles_content)
+        time.sleep(4)
+        menu()
+    elif nav_menu_input.lower() == 'c':
+        time.sleep(.5)
+        check_new_title()
+        time.sleep(1)
+        menu()
+    elif nav_menu_input.lower() == 'd':
+        time.sleep(.5)
+        print("Forty-two")
+        time.sleep(1)
+        menu()
+    elif nav_menu_input.lower() == 'e':
+        quit
+    else:
+        time.sleep(.5)
+        print('Where did you learn how to type? Novice School??')
+        time.sleep(1)
+        menu()
+
+menu()
